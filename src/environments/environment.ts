98130c6cf64657d39079ada515067f7178643822
Jordan LAMBERT <71871266+JordanLmb@users.noreply.github.com>
2021-12-11 18:30:22 +0100

    Ajout de firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBhaQ3L_oAybc0OWIJ_P2zOZXEjP9LPo6U",
+    authDomain: "projet-awi-85acc.firebaseapp.com",
+    databaseURL: "https://projet-awi-85acc-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "projet-awi-85acc",
+    storageBucket: "projet-awi-85acc.appspot.com",
+    messagingSenderId: "257935858065",
+    appId: "1:257935858065:web:f10cbdfdfed480cd67e230",
+    measurementId: "G-RDLJ8NWV66"
+  }
 };
 
 /*
